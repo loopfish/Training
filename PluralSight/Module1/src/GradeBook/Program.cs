f94526d661c4391de964a8586c9f3c2498e61da3
Sam Kompfner <adm.samuel.kompfner@sra.org.uk>
2021-01-05 11:08:23 +0000

    add module1

---
+++ PluralSight/Module1/src/GradeBook/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book = new InMemoryBook("Sam's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book names {book.Name}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
